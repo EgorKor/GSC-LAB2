03fc75fc594bb22aa2a750ad6b69cd4feb680e6b
Егор <ekorovin03@gmail.com>
2023-10-11 13:46:36 +0400

    second Lab final vesion - no bufferization

--- GSC_LAB2/Form1.Designer.cs
+++ GSC_LAB2/Form1.Designer.cs
@@ -36,6 +36,7 @@
             // 
             pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             pictureBox1.BackColor = SystemColors.ButtonHighlight;
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
             pictureBox1.Location = new Point(-1, 0);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(1014, 336);
@@ -47,8 +48,8 @@
             // 
             button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             button1.BackColor = SystemColors.Control;
-            button1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            button1.Location = new Point(848, 396);
+            button1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(848, 376);
             button1.Name = "button1";
             button1.Size = new Size(155, 46);
             button1.TabIndex = 1;
@@ -59,40 +60,43 @@
             // colorComboBox
             // 
             colorComboBox.Anchor = AnchorStyles.Bottom;
-            colorComboBox.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            colorComboBox.BackColor = SystemColors.MenuBar;
+            colorComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            colorComboBox.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
             colorComboBox.FormattingEnabled = true;
             colorComboBox.Items.AddRange(new object[] { "Зелёный", "Красный", "Жёлтый", "Синий" });
             colorComboBox.Location = new Point(24, 396);
             colorComboBox.Name = "colorComboBox";
             colorComboBox.Size = new Size(243, 38);
             colorComboBox.TabIndex = 2;
-            colorComboBox.Text = "Цвет закрашивания";
             colorComboBox.SelectedIndexChanged += colorComboBox_SelectedIndexChanged;
             // 
             // visualModeComboBox
             // 
             visualModeComboBox.Anchor = AnchorStyles.Bottom;
-            visualModeComboBox.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            visualModeComboBox.BackColor = SystemColors.MenuBar;
+            visualModeComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            visualModeComboBox.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
             visualModeComboBox.FormattingEnabled = true;
-            visualModeComboBox.Items.AddRange(new object[] { "с граничным многоугольником", "без граничного многоугольника" });
+            visualModeComboBox.Items.AddRange(new object[] { "C граничным многоугольником", "Без граничного многоугольника" });
             visualModeComboBox.Location = new Point(294, 396);
             visualModeComboBox.Name = "visualModeComboBox";
             visualModeComboBox.Size = new Size(391, 38);
             visualModeComboBox.TabIndex = 3;
-            visualModeComboBox.Text = "Режим визуализации";
             visualModeComboBox.SelectedIndexChanged += visualModeComboBox_SelectedIndexChanged;
             // 
             // algoritmChoseComboBox
             // 
             algoritmChoseComboBox.Anchor = AnchorStyles.Bottom;
-            algoritmChoseComboBox.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            algoritmChoseComboBox.BackColor = SystemColors.MenuBar;
+            algoritmChoseComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            algoritmChoseComboBox.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
             algoritmChoseComboBox.FormattingEnabled = true;
             algoritmChoseComboBox.Items.AddRange(new object[] { "Для ориентированного многоугольника", "Для неориентированного многоугольника" });
             algoritmChoseComboBox.Location = new Point(24, 352);
             algoritmChoseComboBox.Name = "algoritmChoseComboBox";
             algoritmChoseComboBox.Size = new Size(661, 38);
             algoritmChoseComboBox.TabIndex = 4;
-            algoritmChoseComboBox.Text = "Алгоритм закрашивания";
             algoritmChoseComboBox.SelectedIndexChanged += algoritmChoseComboBox_SelectedIndexChanged;
             // 
             // Form1
--- GSC_LAB2/Form1.cs
+++ GSC_LAB2/Form1.cs
@@ -2,19 +2,19 @@
 {
     public partial class Form1 : Form
     {
-        private Graphics graphics;
-        private List<Point> pointList;
-        private Pen drawBorderPen;
-        private Pen paintingPen;
-        private int pointCounter;
-        private int algoritmCode;
-        private const int ORIENTED_ALGORITHM = 0;
-        private const int NON_ORIENTED_ALGORITHM = 1;
+        private Graphics graphics;      //Объект графики, в котором происходит отрисовка
+        private List<Point> pointList;  //Список точек
+        private Pen drawBorderPen;      //Ручка для отрисовки границ
+        private Pen paintingPen;        //Ручка для закрашивания
+        private int pointCounter;       //Счётчик точек
+        private int algoritmCode;       //Код выбранного алгоритма закрашивания
+        private const int ORIENTED_ALGORITHM = 0;     //константа - ориентированный алгоритм
+        private const int NON_ORIENTED_ALGORITHM = 1; //константа - неориентированный алгоритм
 
-        private int visualMode;
-        private const int BORDER_MODE = 0;
-        private const int NON_BORDER_MODE = 1;
-         
+        private int visualMode;                //Код выбранного режима визуализации
+        private const int BORDER_MODE = 0;     //Константа режима с границами
+        private const int NON_BORDER_MODE = 1; //Константа режима без границ
+
 
         public Form1()
         {
@@ -25,19 +25,28 @@
             pointList = new List<Point>();
             drawBorderPen = new Pen(Color.Black, 1);
             paintingPen = new Pen(Color.Green, 1);
+            visualModeComboBox.SelectedIndex = 0;
+            algoritmChoseComboBox.SelectedIndex = 0;
+            colorComboBox.SelectedIndex = 0;
         }
 
         private void paint_with__non_oriented_algoritm()
-        { 
+        {
+            /*Вычисляем Ymin и Ymax*/
             int Ymin = pointList.Min(point => point.Y);
             int Ymax = pointList.Max(point => point.Y);
             Ymin = Math.Max(Ymin, 0);
             Ymax = Math.Min(Ymax, pictureBox1.Height - 1);
-            List<int> Xb = new List<int>();
+            List<int> Xb = new List<int>();//список X-сов - пересечений многоугольника
+            /*Закрашивание линий на которых лежит многоугольник*/
             for (int Y = Ymin; Y <= Ymax; Y++)
             {
-                Xb.Clear();
-                for(int i = 0; i < pointCounter; i++)
+                Xb.Clear();//очищаем список
+                /*Последовально просматривая все прямые
+                 образующие многоугольник, находим X для точек пересечения
+                 с текущей прямой по Y 
+                 */
+                for (int i = 0; i < pointCounter; i++)
                 {
                     int k;
                     if (i < pointCounter - 1)
@@ -45,162 +54,183 @@
                     else
                         k = 0;
 
-                   if ((pointList.ElementAt(i).Y < Y && pointList.ElementAt(k).Y >= Y)
-                        || 
-                        (pointList.ElementAt(i).Y >= Y && pointList.ElementAt(k).Y < Y))
+                    if ((pointList[i].Y < Y && pointList[k].Y >= Y) ||
+                        (pointList[i].Y >= Y && pointList[k].Y < Y))
+                    //Если прямая пересекает многоугольник
                     {
-                        Xb.Add(calculateXonY(pointList.ElementAt(i), pointList.ElementAt(k), Y));
+                        //Вычисляем Х пересечения и добавляем в список
+                        Xb.Add(calculateXonY(pointList[i], pointList[k], Y));
                     }
                 }
-                Xb.Sort();
+                Xb.Sort();//Сортируем список Х-сов
+                /*Последовательно беря пары Х-сов из списков закрашиваем */
                 for (int j = 0; j < Xb.Count; j += 2)
                 {
-                    graphics.DrawLine(paintingPen, new Point(Xb.ElementAt(j), Y), new Point(Xb.ElementAt(j + 1), Y));
+                    graphics.DrawLine(paintingPen, new Point(Xb[j], Y), new Point(Xb.ElementAt(j + 1), Y));
                 }
             }
-        } 
-        
+        }
+
+        /*  Метод вычисляющий точку 
+            Х пересечения прямой по друм точкам
+            с прямой заданной Y       
+         */
         private int calculateXonY(Point pt1, Point pt2, int y)
         {
-            return (((y - pt1.Y) * (pt2.X - pt1.X))) / (pt2.Y - pt1.Y) + pt1.X;
+            return (y - pt1.Y) * (pt2.X - pt1.X) / (pt2.Y - pt1.Y) + pt1.X;
         }
 
         private void paint_with_oriented_algoritm()
         {
-           
-            int Ymin = pointList.ElementAt(0).Y;
-            int Ymax = pointList.ElementAt(0).Y;
+            /*Находим Ymax, Ymin, индекс j для вершины Ymax*/
+            int Ymin = pointList[0].Y;
+            int Ymax = pointList[0].Y;
             int j = 0;
-            for(int i = 0; i < pointList.Count; i++)
+            for (int i = 0; i < pointList.Count; i++)
             {
-                if(pointList.ElementAt(i).Y > Ymax)
+                if (pointList[i].Y > Ymax)
                 {
-                    Ymax = pointList.ElementAt(i).Y;
+                    Ymax = pointList[i].Y;
                     j = i;
                 }
-                if(pointList.ElementAt(i).Y < Ymin)
-                        Ymin = pointList.ElementAt(i).Y;
+                if (pointList[i].Y < Ymin)
+                    Ymin = pointList[i].Y;
             }
             Ymin = Math.Max(0, Ymin);
             Ymax = Math.Min(Ymax, pictureBox1.Height - 1);
-            int j_minus_one = j == 0 ? pointList.Count - 1 : j - 1;
-            int j_plus_one = j == pointList.Count - 1 ? 0 : j + 1;
-            int S = detThree(new int[][] {
-            new int[]{pointList.ElementAt(j_minus_one).X, pointList.ElementAt(j_minus_one).Y, 1},
-            new int[]{pointList.ElementAt(j).X, pointList.ElementAt(j).Y, 1},
-            new int[]{pointList.ElementAt(j_plus_one).X, pointList.ElementAt(j_plus_one).Y, 1} }) / 2;
-            bool CW = S < 0 ? true : false;
-            if (CW)
-            {
-                for(int Y = 0; Y < Ymin; Y++)
-                {
-                    graphics.DrawLine(paintingPen, new Point(0, Y), new Point(pictureBox1.Width - 1, Y));
-                }
-            }
-            List<int> Xl = new List<int>();
-            List<int> Xr = new List<int>();
-            for(int Y = Ymin; Y <= Ymax; Y++)
+            int j_minus_one = j == 0 ? pointList.Count - 1 : j - 1;//задаем индекс j - 1
+            int j_plus_one = j == pointList.Count - 1 ? 0 : j + 1; //задаем индекс j + 1
+            int S = detThree(new int[][] {//вычисляем площадь через опредитель
+                new int[]{pointList[j_minus_one].X, pointList[j_minus_one].Y, 1},
+                new int[]{pointList[j].X, pointList[j].Y, 1},
+                new int[]{pointList[j_plus_one].X, pointList[j_plus_one].Y, 1} }) / 2;
+            bool CW = S < 0 ? true : false;//Флаг ориентированности
+            if (CW)//Если ориентация против часовой
+                paintArea(0, Ymin);//закрашиваем верхнюю область до многоугольника
+            List<int> Xl = new List<int>();//список левых Х-сов
+            List<int> Xr = new List<int>();//список правых Х-сов
+            /*Закрашивание линий на которых лежит многоугольник*/
+            for (int Y = Ymin; Y <= Ymax; Y++)
             {
                 Xl.Clear();
                 Xr.Clear();
-                for(int i = 0; i < pointCounter; i++)
+                /*Последовально просматривая все прямые
+                  образующие многоугольник, находим X для точек пересечения
+                  с текущей прямой по Y 
+                 */
+                for (int i = 0; i < pointCounter; i++)
                 {
                     int k;
                     if (i < pointCounter - 1)
                         k = i + 1;
                     else
                         k = 0;
-                    if ((pointList.ElementAt(i).Y < Y && pointList.ElementAt(k).Y >= Y)
+                    if ((pointList[i].Y < Y && pointList[k].Y >= Y)
                         ||
-                        (pointList.ElementAt(i).Y >= Y && pointList.ElementAt(k).Y < Y))
-                    {
-                        int X = calculateXonY(pointList.ElementAt(i), pointList.ElementAt(k), Y);
-                        if (pointList.ElementAt(k).Y - pointList.ElementAt(i).Y > 0)
+                        (pointList[i].Y >= Y && pointList[k].Y < Y))
+                    {//Если прямая пересекает многоугольник
+                        //Вычисляем Х пересечения 
+                        int X = calculateXonY(pointList[i], pointList[k], Y);
+                        if (pointList[k].Y - pointList[i].Y > 0)//Если Х левый
                             Xr.Add(X);
-                        else
+                        else //Если Х левый
                             Xl.Add(X);
-                    }   
+                    }
                 }
-                if (CW)
+                if (CW)//Если ориентация против часовой
                 {
-                    Xl.Add(0);
-                    Xr.Add(pictureBox1.Width - 1);
+                    Xl.Add(0);//Добавляем X - начало области рисования
+                    Xr.Add(pictureBox1.Width - 1);//Добавляем X - конец области рисования
                 }
-                Xl.Sort();
-                Xr.Sort();
-                for(int i = 0; i < Xl.Count; i++)
+                Xl.Sort();//Сортируем список левых Х
+                Xr.Sort();//Сортируем список правых Х
+                /*Последовательно беря пары Х-сов из списков закрашиваем*/
+                for (int i = 0; i < Xl.Count; i++)
                 {
-                    if(Xl.ElementAt(i) < Xr.ElementAt(i))
+                    if (Xl[i] < Xr[i])
                     {
-                        graphics.DrawLine(paintingPen, new Point(Xl.ElementAt(i), Y), new Point(Xr.ElementAt(i),Y));
+                        graphics.DrawLine(paintingPen, new Point(Xl[i], Y), new Point(Xr[i], Y));
                     }
                 }
             }
-            if (CW)
-            {
-                for (int Y = Ymax + 1; Y < pictureBox1.Height - 1; Y++)
-                {
-                    graphics.DrawLine(paintingPen, new Point(0, Y), new Point(pictureBox1.Width - 1, Y));
-                }
-            }
+            if (CW)//Если ориентация - против часовой
+                //Закрашиваем нижнюю область под многоугольником
+                paintArea(Ymax + 1, pictureBox1.Height - 1);
         }
 
+        /*Метод вычисления определителя 3-го порядка*/
         private int detThree(int[][] matrix)
         {
-            return matrix[0][0] * matrix[1][1] * matrix[2][2]
-                + matrix[1][0] * matrix[2][1] * matrix[0][2]
-                + matrix[0][1] * matrix[1][2] * matrix[2][0]
-                - matrix[0][2] * matrix[1][1] * matrix[2][0]
-                - matrix[0][1] * matrix[2][2] * matrix[1][0]
-                - matrix[0][0] * matrix[1][2] * matrix[2][1];
+            return matrix[0][0] * matrix[1][1] * matrix[2][2] +
+                 matrix[1][0] * matrix[2][1] * matrix[0][2] +
+                 matrix[0][1] * matrix[1][2] * matrix[2][0] -
+                 matrix[0][2] * matrix[1][1] * matrix[2][0] -
+                 matrix[0][1] * matrix[2][2] * matrix[1][0] -
+                 matrix[0][0] * matrix[1][2] * matrix[2][1];
+        }
+        /*Метод закраски области рисования по двум заданным Y-кам*/
+        private void paintArea(int Ybegin, int Yend)
+        {
+            for (int Y = Ybegin; Y < Yend; Y++)
+            {
+                graphics.DrawLine(paintingPen, new Point(0, Y), new Point(pictureBox1.Width - 1, Y));
+            }
         }
 
+        /*Обработчик события - нажатие мыши на области рисования*/
         private void pictureBox_MouseDown(object sender, MouseEventArgs e)
         {
-            if (e.Button == MouseButtons.Left)
+            if (e.Button == MouseButtons.Left)//Если нажата ЛКМ
             {
-                pointList.Add(new Point(e.X, e.Y));
-                if (pointCounter > 0)
+                pointList.Add(new Point(e.X, e.Y));//Добавляем точку в список вершин многоугольника
+                if (pointCounter > 0)//Если точек больше нуля
+                {
+                    if (visualMode == BORDER_MODE)//Если режим с визуализацией - рисуем границы
+                        graphics.DrawLine(drawBorderPen, pointList.ElementAt(pointCounter - 1), pointList[pointCounter]);
+                }
+                if (visualMode == BORDER_MODE)//Если режим с визуализацией - рисуем точки
+                    drawPoint(e.X, e.Y);
+                pointCounter++;//изменяем счётчик точек
+            }
+            else//Если нажата ПКМ
+            {
+                if (pointCounter > 2)//Если достаточно точек для закрашивания
                 {
                     if (visualMode == BORDER_MODE)
-                        graphics.DrawLine(drawBorderPen, pointList.ElementAt(pointCounter - 1), pointList.ElementAt(pointCounter));
-                }
-                if (visualMode == BORDER_MODE)
-                    drawPoint(e.X, e.Y);
-                pointCounter++;
-            }
-            else
-            {
-                if (pointCounter > 2)
-                {
-                    if(visualMode == BORDER_MODE)
                         graphics.DrawLine(drawBorderPen, pointList.ElementAt(0), pointList.ElementAt(pointCounter - 1));
-                    switch (algoritmCode)
+                    switch (algoritmCode)//проверяем какой алгоритм закрашивания выбран
                     {
                         case ORIENTED_ALGORITHM:
+                            /*рисуем методом для ориентированных многоугольников*/
                             paint_with_oriented_algoritm();
                             break;
                         case NON_ORIENTED_ALGORITHM:
+                            /*рисуем методом для неориентированных многоугольников*/
                             paint_with__non_oriented_algoritm();
                             break;
                     }
-                    pointCounter = 0;
-                    pointList.Clear();
+                    /*После отрисовки*/
+                    pointCounter = 0;//зануляем счётчик точек
+                    pointList.Clear();//очищаем список точек
                 }
-                else
+                else//Если недостаточно точек для рисования
                 {
                     MessageBox.Show("Вы не можете рисовать, т.к. точек меньше 2", "Ошибка");
                 }
             }
         }
 
+        /*Обработчик события - изменение размера формы*/
         private void formSizeChangedListener(object sender, EventArgs e)
         {
             graphics.Clear(Color.White);
             graphics = pictureBox1.CreateGraphics();
             graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            pointCounter = 0;
+            pointList.Clear();
         }
 
+        /*Обработчик событие - нажатие на кнопку "Очистить"*/
         private void btClear_Click(object sender, EventArgs e)
         {
             graphics.Clear(Color.White);
@@ -208,7 +238,7 @@
             pointList.Clear();
         }
 
-
+        /*Метод рисования точки*/
         private void drawPoint(int x, int y)
         {
             graphics.DrawEllipse(drawBorderPen, x - 2, y - 2, 5, 5);
@@ -228,13 +258,17 @@
         {
             switch (colorComboBox.SelectedIndex)
             {
-                case 0: paintingPen.Color = Color.Green;
+                case 0:
+                    paintingPen.Color = Color.Green;
                     break;
-                case 1: paintingPen.Color = Color.Red;
+                case 1:
+                    paintingPen.Color = Color.Red;
                     break;
-                case 2: paintingPen.Color = Color.Yellow;
+                case 2:
+                    paintingPen.Color = Color.Yellow;
                     break;
-                case 3: paintingPen.Color = Color.Blue;
+                case 3:
+                    paintingPen.Color = Color.Blue;
                     break;
             }
         }
